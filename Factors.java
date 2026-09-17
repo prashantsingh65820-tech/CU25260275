1e6e4a014ae0e15ce18156d6d25b4dce808fd337
Prashant Singh <prashantsingh65820@gmail.com>
2026-09-17 19:19:09 +0530

    do-while loop
    Creating a java program using a do-while loop to display all factors of a number .

---
+++ Factors.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        do {
+            if (n % i == 0)
+                System.out.print(i + " ");
+
+            i++;
+        } while (i <= n);
+    }
+}
