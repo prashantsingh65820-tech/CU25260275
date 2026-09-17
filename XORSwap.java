0905bedbfd32afc424a57eaab5492c2836b1bb65
Prashant Singh <prashantsingh65820@gmail.com>
2026-09-17 16:30:50 +0530

    Add files via upload
    Using Bitwise operator ( XOR ) to swap two integers without using a temporary variables

---
+++ XORSwap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class XORSwap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping:");
+        System.out.println("First = " + a);
+        System.out.println("Second = " + b);
+    }
+}
