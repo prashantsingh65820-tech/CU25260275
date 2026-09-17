2f47e5602ebb856492cc3b2712781a90790a7a0c
Prashant Singh <prashantsingh65820@gmail.com>
2026-09-17 16:36:24 +0530

    Add files via upload
    Using Bitwise operator to count the number of set bits (1s) in the binary representation.

---
+++ SetBits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n != 0) {
+            count += n & 1;
+            n = n >>> 1;
+        }
+
+        System.out.println("Number of set bits = " + count);
+    }
+}
