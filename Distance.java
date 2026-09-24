d1c161998bff2ea83740f3387199a84f51f0d4c1
Prashant Singh <prashantsingh65820@gmail.com>
2026-09-24 19:34:23 +0530

    Arithmetic Operation
    Developed a Java program to find the distance between two points in a 2D plane using the Distance formula.

---
+++ Distance.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x1 and y1: ");
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+
+        System.out.print("Enter x2 and y2: ");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        double distance = Math.sqrt(
+            Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+        );
+
+        System.out.println("Distance = " + distance);
+    }
+}
